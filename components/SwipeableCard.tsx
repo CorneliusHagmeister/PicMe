8ad217c06cb777f90ebf6a52cb00404ba810f003
Cornelius Hagmeister <cornelius.hagmeister@googlemail.com>
2024-07-25 22:25:43 +0100

    Temporary Commit at 25/07/2024, 22:25:43
    Summary:
    
    Test Plan:

--- components/SwipeableCard.tsx
+++ components/SwipeableCard.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { Card, CardBody } from "@nextui-org/react";
-import { motion, useDragControls } from "framer-motion";
+import { Card } from "@nextui-org/react";
+import { motion } from "framer-motion";
 
 const sliderTransition = {
   duration: 0.5,
@@ -35,7 +35,7 @@ export default function SwipeableCard({
   };
   return (
     <motion.div
-      className={`flex-grow flex absolute top-0 right-0 left-0 bottom-0 `}
+      className={`flex-grow flex absolute top-0 right-0 left-0 bottom-0 ${className}`}
       initial={true}
       drag
       dragElastic={1}
